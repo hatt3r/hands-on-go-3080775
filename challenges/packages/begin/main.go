14a140e0e13f17bcf96f6aaab43299ed2bfa6da8
Serene Mathew <46328829+hatt3r@users.noreply.github.com>
2026-04-01 00:04:11 +0000

    packages and functions chapter

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -2,9 +2,16 @@
 package main
 
 // import the proverbs package
-
+import (
+	"github.com/jboursiquot/go-proverbs" 
+	"fmt"
+	
+)
 // getRandomProverb returns a random proverb from the proverbs package
-
+func getRandomProverb () string {
+return proverbs.Random().Saying
+}
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomProverb())
 }
